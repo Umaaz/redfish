a98142a2d63ef926b6240d00fb5fd971c828ab89
Ben Donnelly <b.w.donnelly1@googlemail.com>
2024-04-19 22:44:43 +0100

    fix(json): improve data source to allow formatting
     - add defaults for headers

---
+++ pkg/config/pkl/gen/appconfig/Defaults.pkl.go
@@ -0,0 +1,6 @@
+// Code generated from Pkl module `redfish.config.app`. DO NOT EDIT.
+package appconfig
+
+type Defaults struct {
+	Headers map[string]string `pkl:"headers" json:"headers,omitempty" toml:"headers,omitempty" yaml:"headers,omitempty"`
+}
